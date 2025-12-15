737b5129f1f72dc602312d79fb239aec1e8dcbe8
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2025-12-16 02:26:26 +0530

    defined user utility

---
+++ app/src/main/java/booking/ticket/util/UserServiceUtil.java
@@ -0,0 +1,16 @@
+package booking.ticket.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class UserServiceUtil {
+
+
+    public static String hashedPassword(String plainPassword){
+        return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
+    }
+
+
+    public static boolean checkPassword(String plainPassword, String hashedPassword){
+        return BCrypt.checkpw(plainPassword, hashedPassword);
+    }
+}
