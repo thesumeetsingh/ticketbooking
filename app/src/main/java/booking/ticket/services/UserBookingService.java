7992c13278461b113a5243b46903a86af2a5541b
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2025-12-16 01:46:01 +0530

    user login check

--- app/src/main/java/booking/ticket/services/UserBookingService.java
+++ app/src/main/java/booking/ticket/services/UserBookingService.java
@@ -7,19 +7,28 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public class UserBookingService {
 
     private User user;
+    private List<User> userList;
     private static final String USERS_PATH="../localdb/users.json";  //path of the local user database
     private ObjectMapper objectMapper=new ObjectMapper();    //serialise: map data to json, deserialise: map data from json to java
-    private List<User> userList;
 
-    public UserBookingService(User user) throws IOException
+    public UserBookingService(User user1) throws IOException
     {
-        this.user=user;
+        this.user=user1;
         File users=new File(USERS_PATH);
         userList = objectMapper.readValue(users, new TypeReference<List<User>>(){});
 
+    }
+
+    public boolean loginUser(){
+        Optional<User> foundUser=userList.stream().filter( user1->{
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword, user1.getPassword);
+        }).findFirst() ;
+
+        return foundUser.isPresent();
     }
 }
