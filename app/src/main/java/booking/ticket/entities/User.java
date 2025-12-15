975b7dc479a7207c32e6254fb8eadd239bfb6061
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2025-12-16 01:54:10 +0530

    getters and setters for User

--- app/src/main/java/booking/ticket/entities/User.java
+++ app/src/main/java/booking/ticket/entities/User.java
@@ -13,4 +13,53 @@ public class User {
     private String userId;
 
     private List<Ticket> ticketsBooked;
+
+    public User(String name, String password, String hashedPassword, List<Ticket> ticketsBooked, String userId){
+        this.name=name;
+        this.password=password;
+        this.hashedPassword=hashedPassword;
+        this.userId=userId;
+        this.ticketsBooked=ticketsBooked;
+    }
+
+    public User(){}
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getHashedPassword(){
+        return hashedPassword;
+    }
+
+    public List<Ticket> getTicketsBooked(){
+        return ticketsBooked;
+    }
+
+    public void printTicket(){
+        for(int i=0;i<ticketsBooked.size();i++){
+            System.out.println(ticketsBooked.get(i).getTicketInfo());
+        }
+    }
+
+    public String getUserId(){
+        return userId;
+    }
+
+    //setters
+    public void setHashedPassword(String hashedPassword){
+        this.hashedPassword=hashedPassword;
+    }
+
+    public void setTicketsBooked(List<Ticket> ticketsBooked){
+        this.ticketsBooked=ticketsBooked;
+    }
+
+    public void setUserId(String userId){
+        this.userId=userId;
+    }
 }
