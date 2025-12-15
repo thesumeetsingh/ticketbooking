794b0ad83c2f815626f3b837347dc51ac16bbfca
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2025-12-16 02:27:20 +0530

    getters and setters for ticket and train class

--- app/src/main/java/booking/ticket/entities/Ticket.java
+++ app/src/main/java/booking/ticket/entities/Ticket.java
@@ -5,14 +5,41 @@ import java.util.Date;
 public class Ticket {
 
 
-    private String TicketID;
+    private String ticketID;
 
     private String userId;
     private String source;
     private String destination;
 
-    private Date dateOfTravel;
+    private String dateOfTravel;
 
     private Train train;
+
+    public Ticket(){}
+
+    public Ticket(String ticketID, String userId, String source, String destination, String dateOfTravel, Train train){
+        this.ticketID=ticketID;
+        this.userId=userId;
+        this.source=source;
+        this.destination=destination;
+        this.dateOfTravel=dateOfTravel;
+        this.train=train;
+
+    }
+
+    public String getTicketInfo(){
+        return String.format("Ticket ID: %s belongs to User %s from %s to %s on %s", ticketID, userId, source, destination, dateOfTravel);
+    }
+
+
+    public String getTicketID(){
+        return  ticketID;
+    }
+
+    public void setTicketID(String ticketID){
+        this.ticketID=ticketID;
+    }
+
+
 }
     ;
--- app/src/main/java/booking/ticket/entities/Train.java
+++ app/src/main/java/booking/ticket/entities/Train.java
@@ -7,12 +7,74 @@ import java.util.Map;
 public class Train {
 
     private String trainId;
-    private Integer trainNo;
-    private String trainName;  //name of the train
+    private String trainNo;
+//    private String trainName;  //name of the train
 
     private List<List<Integer>> seats;  //2d matrix of seats avaiable with 0 and 1 marked for occupied or not
-    private Map<String, Date> stationTimes;  //map of station name to time of arrival
+    private Map<String, String> stationTimes;  //map of station name to time of arrival
 
     private List<String> stations;  //where the train stops
+
+    public Train(){}
+
+    //getters
+    public Train(String trainId, String trainNo, List<List<Integer>> seats, Map<String, String> stationTimes, List<String> stations){
+        this.trainId=trainId;
+        this.trainNo=trainNo;
+
+        this.seats=seats;
+        this.stationTimes=stationTimes;
+        this.stations=stations;
+    }
+
+    public List<String> getStations(){
+        return stations;
+    }
+
+    public List<List<Integer>> getSeats(){
+        return  seats;
+
+    }
+
+    public String getTrainId(){
+        return trainId;
+    }
+
+    public String getTrainNo(){
+        return trainNo;
+    }
+
+    public Map<String, String > getStationTimes(){
+        return stationTimes;
+    }
+
+    public String getTrainInfo(){
+        return String.format("Train ID: %s Train No: %s", trainId, trainNo);
+    }
+
+    //setters
+    public void setTrainId(){
+        this.trainId=trainId;
+    }
+
+    public void setTrainNo(){
+        this.trainNo=trainNo;
+    }
+
+    public void setSeats(List<List<Integer>> seats){
+        this.seats=seats;
+    }
+
+    public void setStationTimes(Map<String, String> stationTimes){
+        this.stationTimes=stationTimes;
+    }
+
+    public  void setStations(List<String> stations){
+        this.stations=stations;
+    }
+
+
+
+
 
 }
